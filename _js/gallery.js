b51c53af9cc4953fe5ac3a128bc9cd3f248318b6
snackythecat <127311324+snackythecat@users.noreply.github.com>
2023-04-19 18:35:31 +0200

    last

--- _js/gallery.js
+++ _js/gallery.js
@@ -1,34 +1,32 @@
-"use strict"
+"use strict";
 
 /**
  * Selects a random full image at the start and displays it.
  */
 function showRandomImageAtStart() {
-    // TODO: Select all 6 links (<a>) in the thumbnail section. They contain the URLs to the full images.
-    // TODO: Select a random entry out of these 6.
-    // TODO: Implement switchFullImage() below.
-    // TODO: Call switchFullImage() with the URL of the random image and the alt attribute of the thumbnail (it contains the description).
-    // TODO: Set a background color (classes .bg-dark and .text-white) to the card-body of your random image (hint: it's the sibling element of your link).
+  // TODO: Select all 6 links (<a>) in the thumbnail section. They contain the URLs to the full images.
+  // TODO: Select a random entry out of these 6.
+  // TODO: Implement switchFullImage() below.
+  // TODO: Call switchFullImage() with the URL of the random image and the alt attribute of the thumbnail (it contains the description).
+  // TODO: Set a background color (classes .bg-dark and .text-white) to the card-body of your random image (hint: it's the sibling element of your link).
 
-    let section = document.getElementById("thumbnails");
-    let links = section.querySelectorAll("a");
-    //console.log(links);
-    let randomLink = links[Math.floor(Math.random() * links.length)];
-    //console.log(randomLink);
-    let imageUrl = randomLink.href
-    let imageDescription = randomLink.firstElementChild.alt;
+  let section = document.getElementById("thumbnails");
+  let links = section.querySelectorAll("a");
+  //console.log(links);
+  let randomLink = links[Math.floor(Math.random() * links.length)];
+  //console.log(randomLink);
+  let imageUrl = randomLink.href;
+  let imageDescription = randomLink.firstElementChild.alt;
 
-    //console.log(imageUrl);
-    //console.log(imageDescription);
+  //console.log(imageUrl);
+  //console.log(imageDescription);
 
+  switchFullImage(imageUrl, imageDescription);
+  //storeNotes(imageUrl);
+  loadNotes(imageUrl);
 
-    switchFullImage(imageUrl, imageDescription);
-    //storeNotes(imageUrl);
-    loadNotes(imageUrl);
-   
-    let cardBody = randomLink.nextElementSibling;
-    cardBody.classList.add("bg-dark", "text-white");
-    
+  let cardBody = randomLink.nextElementSibling;
+  cardBody.classList.add("bg-dark", "text-white");
 }
 
 /**
@@ -38,73 +36,64 @@ function showRandomImageAtStart() {
  * - Load the notes for the current image.
  */
 function prepareLinks() {
-    // TODO: Select all the 6 links (<a>) in the thumbnail section.
-    // TODO: Set an event listener for the click event on every <a> element.
-    //  (or advanced: think of a way to do it with one single handler)
+  // TODO: Select all the 6 links (<a>) in the thumbnail section.
+  // TODO: Set an event listener for the click event on every <a> element.
+  //  (or advanced: think of a way to do it with one single handler)
 
-    let section = document.getElementById("thumbnails");
-    let links = section.querySelectorAll("a");
-    //console.log(links);
+  let section = document.getElementById("thumbnails");
+  let links = section.querySelectorAll("a");
+  //console.log(links);
 
-    links.forEach(function(link){
+  links.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
 
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
+      links.forEach(function (otherLink) {
+        otherLink.nextElementSibling.classList.remove("bg-dark", "text-white");
+      });
 
-            links.forEach(function(otherLink){
-                otherLink.nextElementSibling.classList.remove("bg-dark", "text-white");
-            });
-
-            this.nextElementSibling.classList.add("bg-dark", "text-white");
-            //console.log(this.href)
-            switchFullImage(this.href, this.firstElementChild.alt);
-            //console.log("Marker")
-            loadNotes(this.href);
-            storeNotes(this.href);
-            event.preventDefault();
-            
-         });
-
+      this.nextElementSibling.classList.add("bg-dark", "text-white");
+      //console.log(this.href)
+      switchFullImage(this.href, this.firstElementChild.alt);
+      //console.log("Marker")
+      loadNotes(this.href);
+      storeNotes(this.href);
+      event.preventDefault();
     });
+  });
 
-     
-
-    // TODO: The callback of the listener should do the following things:
-    //  - Remove the .bg-dark and .text-white classes from the card where it's currently set.
-    //  - Add both classes again to the card where the click happened (hint: "this" contains the very <a> element, where the click happened).
-    //  - Call switchFullImage() with the URL clicked link and the alt attribute of the thumbnail.
-    //  - Implement and then call loadNotes() with the key for the current image (hint: the full image's URL makes an easy and unique key).
-    //  - Prevent the default action for the link (we don't want to follow it).
-
-
+  // TODO: The callback of the listener should do the following things:
+  //  - Remove the .bg-dark and .text-white classes from the card where it's currently set.
+  //  - Add both classes again to the card where the click happened (hint: "this" contains the very <a> element, where the click happened).
+  //  - Call switchFullImage() with the URL clicked link and the alt attribute of the thumbnail.
+  //  - Implement and then call loadNotes() with the key for the current image (hint: the full image's URL makes an easy and unique key).
+  //  - Prevent the default action for the link (we don't want to follow it).
 }
-
-
 
 /**
  * Stores or deletes the updated notes of an image after they have been changed.
  */
 function storeNotes() {
-    // TODO: Select the notes field and add a blur listener.
-    // TODO: When the notes field loses focus, store the notes for the current image in the local storage.
-    // TODO: If the notes field is empty, remove the local storage entry.
-    // TODO: Choose an appropriate key (hint: the full image's URL makes an easy and unique key).
+  // TODO: Select the notes field and add a blur listener.
+  // TODO: When the notes field loses focus, store the notes for the current image in the local storage.
+  // TODO: If the notes field is empty, remove the local storage entry.
+  // TODO: Choose an appropriate key (hint: the full image's URL makes an easy and unique key).
 
-    let notes = document.getElementById("notes"); 
-    console.log(notes.textContent);
-    notes.addEventListener("blur", function(){
-        let key = document.getElementById("fullImage").querySelector("src");
-        console.log(key);
-        if (notes.value !== "") {
-            localStorage.setItem(key, notes.value);
-          } else {
-            localStorage.removeItem(key);
-          }
+  const notes = document.getElementById("notes");
+  console.log(notes.textContent);
 
-         // console.log(localStorage(key));
-    })
+  notes.addEventListener("blur", function () {
+    let key = document.querySelector("img").src;
+    console.log(key);
+    let note = notes.innerHTML;
+    if (notes.value !== "") {
+      localStorage.setItem(key, note);
+    } else {
+      localStorage.removeItem(key, note);
+    }
 
-    
+    // console.log(localStorage(key));
+  });
 }
 
 /**
@@ -114,19 +103,17 @@ function storeNotes() {
  * @param {string} imageDescription The image's description (used for the alt attribute and the figure's caption).
  */
 function switchFullImage(imageUrl, imageDescription) {
-    // TODO: Get the <img> element for the full image. Select it by its class or tag name.
-    // TODO: Set its src and alt attributes with the values from the parameters (imageUrl, imageDescription).
-    // TODO: Select the <figcaption> element.
-    // TODO: Set the description (the one you used for the alt attribute) as its text content.
-    
-    let fullImage = document.getElementById("fullImage");
-    let fullImageImg = fullImage.querySelector("img");
-    fullImageImg.setAttribute("src", imageUrl);
-    let caption = fullImage.querySelector("figcaption");
-   // console.log(caption)
-    caption.textContent = imageDescription;
-   
+  // TODO: Get the <img> element for the full image. Select it by its class or tag name.
+  // TODO: Set its src and alt attributes with the values from the parameters (imageUrl, imageDescription).
+  // TODO: Select the <figcaption> element.
+  // TODO: Set the description (the one you used for the alt attribute) as its text content.
 
+  let fullImage = document.getElementById("fullImage");
+  let fullImageImg = fullImage.querySelector("img");
+  fullImageImg.setAttribute("src", imageUrl);
+  let caption = fullImage.querySelector("figcaption");
+  // console.log(caption)
+  caption.textContent = imageDescription;
 }
 
 /**
@@ -134,20 +121,18 @@ function switchFullImage(imageUrl, imageDescription) {
  * @param {string} key The key in local storage where the entry is found.
  */
 function loadNotes(key) {
-    // TODO: Select the notes field.
-    // TODO: Check the local storage at the provided key.
-    //  - If there's an entry, set the notes field's HTML content to the local storage's content.
-    //  - If there's no entry, set the default text "Enter your notes here!".
+  // TODO: Select the notes field.
+  // TODO: Check the local storage at the provided key.
+  //  - If there's an entry, set the notes field's HTML content to the local storage's content.
+  //  - If there's no entry, set the default text "Enter your notes here!".
 
-    let notes = localStorage.getItem(key) ?? 0; // wenn leer dann wird rechts zugewiesen 
-    //console.log(notes);
-    let field = document.getElementById("notes");
-    if ( notes != 0){
-      field.textContent = notes; //field.innerHTML = notes
-    }
-    
-    
-
+  const field = document.getElementById("notes");
+  const saveNotes = localStorage.getItem(key);
+  if (localStorage.getItem(key) === null || localStorage.getItem(key) == "") {
+    field.innerHTML = "Enter your notes here!";
+  } else {
+    field.innerHTML = saveNotes;
+  }
 }
 
 /**
@@ -157,9 +142,9 @@ function loadNotes(key) {
  * @returns {number} A random integer value between min (included) and max (excluded).
  */
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 /**
